fee01d4c21ed368f2a29b9e2ef0e39f52890ab41
Enrico Tomaz <eru.tomaz@gmail.com>
2025-02-18 19:13:11 -0300

    Exercício - Modulo 11

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    })
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+            .contains('Atlas Fitness Tank')
+            .click()
+
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    })
+
+})
