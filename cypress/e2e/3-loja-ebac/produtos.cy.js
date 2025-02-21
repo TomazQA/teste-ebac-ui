f6ac4a3f8df4b7164312d9289d339ded26a03d94
Enrico Tomaz <eru.tomaz@gmail.com>
2025-02-21 05:12:15 -0300

    criando testes com massa de dados

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,17 +1,61 @@
 /// <reference types="cypress" />
+import produtosPage from '../../support/page-objects/produtos.page'
+
+//import { it } from "mocha"
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     })
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-            .contains('Atlas Fitness Tank')
-            .click()
-
+        produtosPage.buscarProdutoLista('Abominable Hoodie')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
     })
 
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    })
+
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('Zeppelin Yoga Pant')
+        cy.get('.product_title').should('contain', 'Zeppelin Yoga Pant')
+
+    })
+
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Aero Daily Fitness Tee')
+        //cy.get('.button-variable-item-XS').click()
+        //cy.get('.button-variable-item-Blue').click()
+        //cy.get('.plus').click()
+        //cy.get('.single_add_to_cart_button').click()
+        produtosPage.addProdutoCarrinho('M', 'Brown', qtd)
+        cy.get('.woocommerce-message').should('contain', `${qtd} × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.`)
+
+        //cy.get('.woocommerce-message').should('contain', 'foram adicionados no seu carrinho.')
+    })
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[0].tamanho,
+                dados[0].cor,
+                dados[0].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+
+        })
+
+
+        //produtosPage.buscarProduto('Aero Daily Fitness Tee')
+
+        //produtosPage.addProdutoCarrinho('M', 'Brown', qtd)
+        //cy.get('.woocommerce-message').should('contain', `${qtd} × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.`)
+
+        //cy.get('.woocommerce-message').should('contain', 'foram adicionados no seu carrinho.')
+    })
 })
